ca6c41cc286b01b5109e6ab1760294771ac9e7d9
calvickauer <calvickauer@gmail.com>
2022-07-27 19:41:12 -0700

    background and player linked

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ canvas.height = 650;
 const ctx = canvas.getContext('2d');
 console.log(ctx);
 
+
+const spritePlayer = new Image();
+spritePlayer.src = "/Users/calvin/Desktop/general-Assembly/unit-1/MyGame/KandysPaybackGame/playerSprites/idle.png";
+
+const canvasBackground = new Image();
+canvasBackground.src = "/Users/calvin/Desktop/general-Assembly/unit-1/MyGame/KandysPaybackGame/backgroundIMGs/craftpix-633456-free-desert-scrolling-2d-game-backgrounds/_PNG/4/background.png"
+
+function animation(){
+    requestAnimationFrame(animation);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(canvasBackground, 0, 0, canvas.width, canvas.height);
+
+}
+
+animation();
