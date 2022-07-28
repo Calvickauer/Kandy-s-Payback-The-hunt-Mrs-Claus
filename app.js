03b19744d4531f97a3b2781b3b3d6020d557f324
calvickauer <calvickauer@gmail.com>
2022-07-27 20:10:10 -0700

    styling

--- app.js
+++ app.js
@@ -14,10 +14,22 @@ spritePlayer.src = "/Users/calvin/Desktop/general-Assembly/unit-1/MyGame/KandysP
 const canvasBackground = new Image();
 canvasBackground.src = "/Users/calvin/Desktop/general-Assembly/unit-1/MyGame/KandysPaybackGame/backgroundIMGs/craftpix-633456-free-desert-scrolling-2d-game-backgrounds/_PNG/4/background.png"
 
+const player = {
+    width: 265,
+    height: 571,
+
+}
+
+
+
+
+
+
 function animation(){
     requestAnimationFrame(animation);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(canvasBackground, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(spritePlayer, 0, 0, player.width, player.height, 300, 300, 50, 112);
 
 }
 
